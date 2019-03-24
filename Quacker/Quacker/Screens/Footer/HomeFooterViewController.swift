9b094d93afe1f4479ce6c275edeb6c1495047fb1
Witek Bobrowski <witek@bobrowski.com.pl>
2019-03-24 10:31:26 +0100

    Add HomeFooterViewController

---
+++ Quacker/Quacker/Screens/Footer/HomeFooterViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeFooterViewController.swift
+//  Quacker
+//
+//  Created by Witek Bobrowski on 24/03/2019.
+//  Copyright © 2019 Witek Bobrowski. All rights reserved.
+//
+
+import UIKit
+
+class HomeFooterViewController: UIViewController {
+
+    private let infoButtonViewController = NeonButtonViewController()
+    private let quackButtonViewController = NeonButtonViewController()
+
+    private let titleLabel: UILabel = create {
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        $0.textColor = StyleSheet.Color.textColor
+        $0.textAlignment = .center
+        $0.baselineAdjustment = .alignCenters
+        $0.text = "WWDC"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+}
+
+extension HomeFooterViewController {
+
+    private func setupView() {
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+}
